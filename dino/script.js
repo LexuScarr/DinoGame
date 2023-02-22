c810116bc8fab946fda962ffd79481ff65edb4b3
LexuScar <109822500+LexuScarr@users.noreply.github.com>
2023-02-22 17:24:18 +0800

    Add files via upload
    second version

--- dino/script.js
+++ dino/script.js
@@ -1,29 +1,49 @@
-const dino = document.getElementById("dino");
+const dino = document.querySelector(".dino");
 const cactus = document.getElementById("cactus");
-const scoreEl = document.getElementById("score");
+var scoreText = $(".score")[0];
+var score = 0;
+var die = false;
 
-document.addEventListener("keydown", function (event) {
-  jump();
-});
+setInterval(function () {
+  var width = document.documentElement.clientWidth;
+  var posX = $(".dino").offset().left;
+  var posY = $(".dino").offset().top;
 
-function jump() {
-  if (dino.classList != "jump") {
-    dino.classList.add("jump");
+  for (let i = 0; i < $(".cactus").length; i++) {
+    var cactusX = $(".cactus").eq(i).offset().left;
+    var cactusY = $(".cactus").eq(i).offset().top;
+    if (cactusX <= -200) {
+      $(".cactus").eq(i).remove();
+      score += 10; // Increment score when a cactus is removed
+    } else {
+      $(".cactus")
+        .eq(i)
+        .css("right", parseInt($(".cactus").eq(i).css("right")) + 2 + "px");
+    }
+    if (posX >= cactusX - 90 && posX <= cactusX + 90 && posY >= cactusY - 45) {
+      $(".dino").eq(0).remove();
+      die = true;
+    }
+  }
+
+  if (die) {
+    $("#restart").show();
+  }
+  scoreText.innerHTML = "Score: " + score;
+}, 1);
+
+setInterval(function () {
+  $(".cactusArea")[0].innerHTML += '<div class="cactus"></div>';
+}, 1500);
+
+var can = true;
+$(document).keyup(function (e) {
+  if (e.which == 32 && can) {
+    $(".dino")[0].classList.add("jump");
+    can = false;
     setTimeout(function () {
-      dino.classList.remove("jump");
-      score += 1; // прибавляем 1 к счетчику очков после прыжка
-      scoreEl.innerText = score; // обновляем значение счетчика на странице
-    }, 300);
+      $(".dino")[0].classList.remove("jump");
+      can = true;
+    }, 700);
   }
-}
-let score = 0;
-let isAlive = setInterval(function () {
-  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-  let cactusLeft = parseInt(
-    window.getComputedStyle(cactus).getPropertyValue("left")
-  );
-
-  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-    alert("Game Over! Your score: " + score);
-  }
-}, 10);
+});
